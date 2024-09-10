508a6677ab25617b174e0ee59bbde3784302942e
leannec8 <149003503+leannec8@users.noreply.github.com>
2024-09-10 22:01:05 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String s = "chocolate";
+        int  i = 3;
+        i = i + 10;
+        final double d = 8.666;
+        boolean b = true;
+        System.out.println(s + i + " " + d + b);
+        System.out.print(100 + 1);
+        System.out.print(" pieces of " + s);
+    }
+}
